bbec72f96add554e1146c72390f01b8cba715aef
n3rdy <eaadithyaa@gmail.com>
2021-01-26 00:58:08 +0530

    Added NeuralNetwork plan .md

--- NeuralNetwork/neural_network.cpp
+++ NeuralNetwork/neural_network.cpp
@@ -1,8 +1,11 @@
-#include "neural_network.hpp"
+#include "neural_network.h"
 
 #include <gsl/gsl_blas.h>
 
-class NeuralNetwork {
-  
-
-};
+NeuralNetwork::NeuralNetwork(std::vector<int>* shapes) {
+  int len = shapes->size();
+  for(int i = 0; i < len-1; i++) {
+    weights.push_back(gsl_matrix_alloc((*shapes)[i+1], (*shapes)[i]));
+    biases.push_back(gsl_matrix_alloc(1, (*shapes)[i+1]));
+  }
+}
--- NeuralNetwork/neural_network.h
+++ NeuralNetwork/neural_network.h
@@ -1,19 +1,18 @@
-#ifndef DARWIN_NEURAL_NETWORK_HPP
-#define DARWIN_NEURAL_NETWORK_HPP
+#ifndef DARWIN_NEURAL_NETWORK_H
+#define DARWIN_NEURAL_NETWORK_H
 
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
+
 #include <vector>
 
 class NeuralNetwork {
-private:
+ private:
   std::vector<gsl_matrix *> weights;
   std::vector<gsl_matrix *> biases;
-
-public:
-  NeuralNetwork(const int *shape_array, int shape_count);
-  NeuralNetwork(const NeuralNetwork *parent);
-
+ public:
+  NeuralNetwork(std::vector<int>* shapes, int inputCountForEachRepeated);
+  NeuralNetwork(const NeuralNetwork* parent);
   std::vector<double> calculate(std::vector<double> input);
 };
 
